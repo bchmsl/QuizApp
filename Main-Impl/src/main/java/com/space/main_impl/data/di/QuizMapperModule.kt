8ece4fb8928e2d2b2d28a903a2ed83133be8d091
Bachana <bachanamosulishvili@gmail.com>
2023-07-07 00:11:57 +0400

    Extracted Main module

--- Main-Impl/src/main/java/com/space/main_impl/data/di/QuizMapperModule.kt
+++ Main-Impl/src/main/java/com/space/main_impl/data/di/QuizMapperModule.kt
@@ -1,6 +1,7 @@
 package com.space.main_impl.data.di
 
-import com.space.common.model.question.data.QuizSubjectEntityMapper
+import com.space.common.model.question.data.QuizQuestionEntityMapper
+import com.space.common.model.subject.data.QuizSubjectEntityMapper
 import com.space.main_impl.data.local.database.model.user.mapper.QuizUserEntityMapper
 import com.space.main_impl.data.local.database.model.user.mapper.QuizUserSubjectEntityMapper
 import com.space.main_impl.data.remote.model.mapper.QuizQuestionDtoMapper
@@ -8,8 +9,8 @@ import com.space.main_impl.data.remote.model.mapper.QuizSubjectDtoMapper
 import org.koin.dsl.module
 
 val dataMapperModule = module {
-    single { com.space.common.model.question.data.QuizQuestionEntityMapper() }
-    single { com.space.common.model.question.data.QuizSubjectEntityMapper() }
+    single { QuizQuestionEntityMapper() }
+    single { QuizSubjectEntityMapper() }
     single { QuizUserEntityMapper() }
     single { QuizQuestionDtoMapper() }
     single { QuizSubjectDtoMapper() }
